8ad55ae4a93e939a5571ed55b8d37d7f2dbada8f
Alex Millar <QuixoticRocket@gmail.com>
2016-02-01 12:45:53 +0000

    initial project setup

---
+++ UmbracoExperiment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UmbracoExperiment.Startup))]
+namespace UmbracoExperiment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
